6cdb3cec60964ed436e251f1973e12cc2cdfd2af
Bodie Solomon <bodie@synapsegarden.net>
2018-08-25 18:04:46 -0400

    Update to latest resource impl

--- res.cxx
+++ res.cxx
@@ -6,28 +6,35 @@
 #include <sstream>
 #include <string>
 
-#include "lz4.h"
+#include "lz4frame.h"
 
 #include "res.hpp"
 
 namespace res {
-    size_t Res::Len() noexcept(true) { return b_comp_len; }
-
-    const char* Res::Read(char* into, size_t len) noexcept(true) {
-	auto err = LZ4_decompress_safe(
-	  reinterpret_cast<const char*>(buf), into, b_comp_len, len);
-
-	if (err < 0) {
-	    throw "bad stream";
-	}
-
-	return into;
-    }
-
     // TODO: Generate these constants in the phx tool.
     const unsigned char Res::buf[] = {
 #include "dat.txt"
     };
 
-    const size_t Res::b_comp_len = 12;
+    const size_t Res::b_comp_len = 13; // "hello world!\n"
+
+    size_t Res::Len() noexcept(true) {
+	// 	return std::extent<decltype(buf)>::value;
+	// }
+	return b_comp_len;
+    }
+
+    const char* Res::Read(char* into, size_t len) noexcept(false) {
+	// std::memcpy(into, buf, len);
+
+	// auto err = LZ4_decompress_safe(
+	//   reinterpret_cast<const char*>(buf), into, b_comp_len, len);
+
+	// if (err < 0) {
+	//     throw "bad stream";
+	// }
+
+	return into;
+    }
+
 }; // namespace res
