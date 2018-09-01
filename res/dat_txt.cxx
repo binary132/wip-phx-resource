6c563ed3a92a2d641b22a62a90815078c390493f
Bodie Solomon <bodie@synapsegarden.net>
2018-08-31 21:11:22 -0400

    Clang-format resource declaration

--- res/dat_txt.cxx
+++ res/dat_txt.cxx
@@ -1,8 +1,8 @@
 #include "mapper.hpp"
 
 namespace res {
-     const size_t Mapper::dat_txt_len = 12;
-     const unsigned char Mapper::dat_txt[] = {
+    const size_t        Mapper::dat_txt_len = 12;
+    const unsigned char Mapper::dat_txt[]   = {
 #include "dat.txt"
-     };
+    };
 }; // namespace res
